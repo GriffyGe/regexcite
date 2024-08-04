263882c72b0d0687898fb262ff13e399300d4389
Griffy Ge <Yifeii.Ge@outlook.com>
2024-08-04 10:58:39 +0800

    Use stringr rather than r-base to do spliting.

---
+++ R/str_split_one.R
@@ -0,0 +1,24 @@
+#' Split a string.
+#'
+#' @param string A character vector with one element
+#' @inheritParams stringr::str_split
+#'
+#' @return A charater vector
+#' @export
+#'
+#' @examples
+#' x <- "alpha,beta,charlie,delta"
+#' str_split_one(x, pattern = ",")
+#' str_split_one(x, pattern = ",", n = 2)
+
+str_split_one <- function(string, pattern, n = Inf){
+
+  #Type checking
+  stopifnot(is.character(string), length(string) <= 1)
+  if (length(string) == 1){
+    stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
+  }else{
+    character()
+  }
+
+}
